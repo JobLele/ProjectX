97ed839e69423387bd2b35bae0388ff4ce554f91
Sainya Goyal <sainyagoyal0@gmail.com>
2020-09-13 14:54:12 +0530

    boiler-react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowseRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Nav from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <h1>Just to chk whether running or not.Here homepage will come later.</h1>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 

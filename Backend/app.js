256431b907eea0e000333dc68833fd885d27d4c0
Samarth <samarthranjanray@gmail.com>
2020-09-13 16:12:06 +0530

    schemas

--- Backend/app.js
+++ Backend/app.js
@@ -1,21 +1,137 @@
+
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', function(req, res) {
-  res.json({"msg" : "This is how we roll", "err": "There ain't any yet"});
+app.use(cors());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(session({
+  secret: "projectXJobLele.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("Connected to mDB"))
+.catch((e) => console.log("Error :", e));
+mongoose.set("useCreateIndex", true);
+
+const employeeSchema = new mongoose.Schema({
+  email: String,
+  name: String,
+  number: Number,
+  qualification: String,
+  appliedFor: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Job'
+    }
+  ]
+});
+const Employee = mongoose.model("Employee", employeeSchema);
+
+const employerSchema = new mongoose.Schema({
+  email: String,
+  name: String,
+  number: Number,
+  jobsPosted: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Job'
+    }
+  ]
+});
+const Employer = mongoose.model("Employer", employerSchema);
+
+const jobSchema = new mongoose.Schema ({
+  title: String,
+  type: String,
+  description: String,
+  postedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Employer'
+  },
+  postedOn: Date,
+  applicants: [
+    {
+      explanation: String,
+      applicant : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Employee'
+      }
+    }
+  ]
+});
+const Job = mongoose.model("Job", jobSchema);
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String,
+  googleId: String,
+  secret: String
 });
 
-app.get("/secrets", function(req, res){
-  let obj = {
-    name : "bot",
-    uid : "69XX69",
-    job : "dilwado"
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: `http://localhost:${process.env.PORT}/auth/google/secrets`,
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
   }
-  res.json({"user" : obj})
+));
+
+
+app.get('/', function(req, res) {
+
 });
 
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    // get employee/employer detail from db and send in res.json
+    res.json({"user": null});
+  });
 
-app.listen(3000, function() {
+
+app.listen(PORT, function() {
   console.log("Server started on port 3000.");
 });

27113c6268e41f21511fb6fbc241d1d9823d8877
Samarth <samarthranjanray@gmail.com>
2020-09-13 22:12:44 +0530

    lol

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Nav from './Components/Navbar/Navbar';
+import Avv from './Components/Navbar/Navbar';
 import Login from './Components/Login/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import Footer from './Components/Footer/Footer';
 
 
 const App = () => {
   return (<div>
     <Router>
-      <Nav/>
+      <Avv />
       <Switch>
         <Route path="/login" component={Login}></Route>
       </Switch>

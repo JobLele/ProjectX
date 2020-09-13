508bb07306c9c9542ced9ffbcc3b178c392a6f81
Sainya Goyal <sainyagoyal0@gmail.com>
2020-09-13 21:18:34 +0530

    login-routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-// import Nav from './Components/Navbar/Navbar';
+import Nav from './Components/Navbar/Navbar';
+import Login from './Components/Login/Login';
 // import Footer from './Components/Footer/Footer';
 
-const App=()=> {
-  return (
-  
-    
-      <div className="App">
+
+const App = () => {
+  return (<div>
+    <Router>
+      <Nav/>
+      <Switch>
+        <Route path="/login" component={Login}></Route>
+      </Switch>
+    </Router>
+     
+      
+      
+         
+
+          <h1>Just to chk whether running or not.Here homepage will come later.</h1>
+
        
-        <h1>Just to chk whether running or not.Here homepage will come later.</h1>
-       
-      </div>
-   
     
+    </div>
   );
 }
 
-export default App;
+export default App

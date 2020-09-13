a4e124380fa3b772e55beaaeda856cb00f0786bc
Sainya Goyal <sainyagoyal0@gmail.com>
2020-09-13 15:07:07 +0530

    react-ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
-import { BrowseRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Nav from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer';
+// import Nav from './Components/Navbar/Navbar';
+// import Footer from './Components/Footer/Footer';
 
-function App() {
+const App=()=> {
   return (
-    <Router>
+  
+    
       <div className="App">
-        <Nav />
+       
         <h1>Just to chk whether running or not.Here homepage will come later.</h1>
-        <Footer />
+       
       </div>
-    </Router>
+   
+    
   );
 }
 

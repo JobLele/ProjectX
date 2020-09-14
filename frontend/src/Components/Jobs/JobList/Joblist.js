73651ee2c3703ed10bfca81233e9d22abc0e1162
Tanishqsoni <tanishqsoni3112@gmail.com>
2020-09-14 14:54:48 +0530

    jl

--- frontend/src/Components/Jobs/JobList/Joblist.js
+++ frontend/src/Components/Jobs/JobList/Joblist.js
@@ -9,34 +9,6 @@ class JobList extends Component {
     render() {
         return (<div>
             <Container>
-<<<<<<< HEAD
-                <Card style={{ width: '18rem' }}>
-                    <div className="date-box">9 oct</div>
-                    <Card.Img variant="top" className="img-box" src="https://lh3.googleusercontent.com/proxy/5JyNbppZ3y4G00tEVWvUShBPbKcYooOSns4PUl2NAhQlzbWhEtb10I50EJYADONVqDTd7zHyrYy2TCBYEanZoJ1qwAYR1GeAFl4GTA" />
-                    <Card.Body>
-                        <Card.Title>Cook</Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Permanent</Card.Subtitle>
-                        <Card.Subtitle className="lg-2 salary">$40/month</Card.Subtitle>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Card.Text>
-                        <div className="bottom-box">
-                            <div>7A 1gokuldam society ,mumbai</div>
-                            <div><Button variant="dark" >view</Button></div>
-                        
-                        </div>
-                        </Card.Text>
-                       
-
-                       
-                        
-                    </Card.Body>
-                </Card>
-
-                                
-=======
                 <div className="card-main-box">
                     <div className="individual-card">
                         <Card style={{ width: '20rem' }}>
@@ -151,7 +123,6 @@ class JobList extends Component {
                 </div>
 
 
->>>>>>> 3c41b1d03378194cc2d5dfad73ef525d730bf214
             </Container>
 
         </div>)

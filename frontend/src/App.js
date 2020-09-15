4068baf0238ffb635455c330b15aa594ae31b313
RithikSinghai <desmondrr22@gmail.com>
2020-09-15 22:44:29 +0530

    kuchtohkiya

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-// import Nav from './Components/Navbar/Navbar';
-// import Footer from './Components/Footer/Footer';
-
-const App=()=> {
-  return (
-  
-    
-      <div className="App">
-       
-        <h1>Just to chk whether running or not.Here homepage will come later.</h1>
-       
-      </div>
-   
-    
-=======
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './Components/Navbar/Navbar';
@@ -43,7 +24,6 @@ const App = () => {
       </Switch>
     </Router>  
     </div>
->>>>>>> bc8ed00d2dcf9ba1393d17bac538bd6b673bcabb
   );
 }
 

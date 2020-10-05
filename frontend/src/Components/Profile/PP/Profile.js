68f8177ea83c955e4e6cda515760f6ab8c074e02
Tanishqsoni <tanishqsoni3112@gmail.com>
2020-10-05 14:41:39 +0530

    profile

---
+++ frontend/src/Components/Profile/PP/Profile.js
@@ -0,0 +1,132 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+
+Name       : Fervency 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20131006
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<span class="icon icon-fire"></span>
+			<h1><a href="#">Fervency</a></h1>
+			<span>Template Design by <a href="http://www.freecsstemplates.org/" rel="nofollow">FreeCSSTemplates.org</a></span> </div>
+		<div id="menu">
+			<ul>
+				<li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
+				<li><a href="#" accesskey="2" title="">Our Clients</a></li>
+				<li><a href="#" accesskey="3" title="">About Us</a></li>
+				<li><a href="#" accesskey="4" title="">Careers</a></li>
+				<li><a href="#" accesskey="5" title="">Contact Us</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div id="wrapper3">
+	<div id="portfolio" class="container">
+		<div class="title">
+			<h2>Design Portfolio</h2>
+			<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span> </div>
+		<div class="column1">
+			<div class="box"> <a href="#"><img src="images/scr01.jpg" alt="" class="image image-full" /></a>
+				<h3>Vestibulum venenatis</h3>
+				<p>Fermentum nibh augue praesent a lacus at urna congue rutrum.</p>
+				<a href="#" class="button button-small">Etiam posuere</a> </div>
+		</div>
+		<div class="column2">
+			<div class="box"> <a href="#"><img src="images/scr02.jpg" alt="" class="image image-full" /></a>
+				<h3>Praesent scelerisque</h3>
+				<p>Vivamus fermentum nibh in augue praesent urna congue rutrum.</p>
+				<a href="#" class="button button-small">Etiam posuere</a> </div>
+		</div>
+		<div class="column3">
+			<div class="box"> <a href="#"><img src="images/scr03.jpg" alt="" class="image image-full" /></a>
+				<h3>Donec dictum metus</h3>
+				<p>Vivamus fermentum nibh in augue praesent urna congue rutrum.</p>
+				<a href="#" class="button button-small">Etiam posuere</a> </div>
+		</div>
+		<div class="column4">
+			<div class="box"> <a href="#"><img src="images/scr04.jpg" alt="" class="image image-full" /></a>
+				<h3>Mauris vulputate dolor</h3>
+				<p>Rutrum fermentum nibh in augue praesent urna congue rutrum.</p>
+				<a href="#" class="button button-small">Etiam posuere</a> </div>
+		</div>
+	</div>
+</div>
+<div id="wrapper1">
+	<div id="welcome" class="container">
+		<div class="title">
+			<h2>Welcome to our website</h2>
+			<span class="byline">Mauris vulputate dolor sit amet nibh</span> </div>
+		<div class="content">
+			<p>This is <strong>Fervency</strong>, a free, fully standards-compliant CSS template designed by <a href="http://www.freecsstemplates.org/" rel="nofollow">FreeCSSTemplates.org</a>. The photos in this template are from <a href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attributions 3.0</a> license, so you are pretty much free to do whatever you want with it (even use it commercially) provided you keep the links in the footer intact. Aside from that, have fun with it :) </p>
+			<a href="#" class="button">Etiam posuere</a> </div>
+	</div>
+</div><div id="wrapper2">
+	<div id="newsletter" class="container">
+		<div class="title">
+			<h2>Newsletter Signup</h2>
+			<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span> </div>
+		<div class="content">
+			<form method="post" action="#">
+				<div class="row half">
+					<div class="6u">
+						<input type="text" class="text" name="name" placeholder="Name" />
+					</div>
+					<div class="6u">
+						<input type="text" class="text" name="email" placeholder="Email" />
+					</div>
+				</div>
+				<div class="row half">
+					<div class="12u">
+						<textarea name="message" placeholder="Message"></textarea>
+					</div>
+				</div>
+				<div class="row">
+					<div class="12u"> <a href="#" class="button submit">Send Message</a> </div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+<div id="wrapper4">
+	<div id="footer" class="container">
+		<div>
+			<header class="title">
+				<h2>Get in touch</h2>
+				<span class="byline">Phasellus nec erat sit amet nibh pellentesque congue</span> </header>
+			<ul class="contact">
+				<li><a href="#" class="icon icon-twitter"><span>Twitter</span></a></li>
+				<li><a href="#" class="icon icon-facebook"><span></span></a></li>
+				<li><a href="#" class="icon icon-dribbble"><span>Pinterest</span></a></li>
+				<li><a href="#" class="icon icon-tumblr"><span>Google+</span></a></li>
+				<li><a href="#" class="icon icon-rss"><span>Pinterest</span></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div id="copyright">
+	<p>Copyright (c) 2013 Sitename.com. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://www.freecsstemplates.org/" rel="nofollow">FreeCSSTemplates.org</a>.</p>
+</div>
+</body>
+</html>
